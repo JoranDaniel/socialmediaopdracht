22fbf2ba70c5e8f176c9e797a5afdf2a23767bc5
JoranDaniel <124999321+JoranDaniel@users.noreply.github.com>
2023-06-22 19:17:58 +0200

    Commit voor milan

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -2,15 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCK-N5U95s_dWbW54QWus3tZKVp4P6HIZ4",
-  authDomain: "socialmediaopdracht-10251.firebaseapp.com",
-  projectId: "socialmediaopdracht-10251",
-  storageBucket: "socialmediaopdracht-10251.appspot.com",
-  messagingSenderId: "817959513902",
-  appId: "1:817959513902:web:fe89adb3e00e1f2479d4bf",
-  measurementId: "G-V9C2NP02D5"
+  apiKey: "AIzaSyCD6grIwkNB4aQyK_Oq4OYCfU31y-LFaeQ",
+  authDomain: "socialmediav2-6287b.firebaseapp.com",
+  projectId: "socialmediav2-6287b",
+  storageBucket: "socialmediav2-6287b.appspot.com",
+  messagingSenderId: "686781341340",
+  appId: "1:686781341340:web:d32dcf83ba3c7427d4d759"
 };
 
 // Initialize Firebase
