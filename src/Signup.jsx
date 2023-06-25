6455a963f5d38330829a647c29a73046e7741cba
JoranDaniel <124999321+JoranDaniel@users.noreply.github.com>
2023-06-25 23:05:10 +0200

    herkansing

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { auth, firestore } from '../src/config/firebase';
+import { useNavigate } from 'react-router-dom';
+import '../src/login.css';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
   const [username, setUsername] = useState('');
   const [signupSuccess, setSignupSuccess] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -15,11 +20,21 @@ const Signup = () => {
     setPassword(event.target.value);
   };
 
+  const handleRepeatPasswordChange = (event) => {
+    setRepeatPassword(event.target.value);
+  };
+
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
 
   const handleSignup = () => {
+    if (password !== repeatPassword) {
+      // Password and repeat password do not match
+      console.error("Password and repeat password do not match");
+      return;
+    }
+
     auth.createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         // User registration successful
@@ -31,6 +46,7 @@ const Signup = () => {
         });
         console.log(user);
         setSignupSuccess(true); // Set signup success state to true
+        navigate('/login'); // Redirect to the login page
       })
       .catch((error) => {
         // Handle error during user registration
@@ -40,17 +56,41 @@ const Signup = () => {
 
   return (
     <div>
-      <h2>Sign Up</h2>
-      {signupSuccess && <p>Sign up successful!</p>}
-      <form>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={handleEmailChange} />
-        <label>Password:</label>
-        <input type="password" value={password} onChange={handlePasswordChange} />
-        <label>Username:</label>
-        <input type="text" value={username} onChange={handleUsernameChange} />
-        <button type="button" onClick={handleSignup}>Sign Up</button>
-      </form>
+      <div className="header">
+        {/* Plaats hier de inhoud van je header */}
+      </div>
+
+      <div className="login-container">
+        <div className="login-form1">
+          <h2 className='titelpagina'>Sign Up</h2>
+          {signupSuccess && <p className="success-message">Sign up successful!</p>}
+          <form className='formvoorlogin'>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" className='inputuser' value={email} onChange={handleEmailChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" className='inputuser' value={username} onChange={handleUsernameChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" className='inputuser' value={password} onChange={handlePasswordChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="repeat-password">Repeat Password:</label>
+              <input type="password" id="repeat-password" className='inputuser' value={repeatPassword} onChange={handleRepeatPasswordChange} />
+            </div>
+           
+            <button className='inputformtype' type="button" onClick={handleSignup}>
+              Sign Up
+            </button>
+            <a href="/login">
+              
+            </a>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
